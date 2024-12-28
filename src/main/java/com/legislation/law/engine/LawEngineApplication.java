2661e89bfbb292dcc5bce1a08257b2317fee3171
Matcl-dev <matheuschavesleite@gmail.com>
2024-12-28 18:31:59 -0300

    first commit

---
+++ src/main/java/com/legislation/law/engine/LawEngineApplication.java
@@ -0,0 +1,13 @@
+package com.legislation.law.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawEngineApplication.class, args);
+	}
+
+}
